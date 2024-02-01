11d48d53cda35831a8a9833845a5e57ec690181b
Mariana Vallejos <marivalleandrea@gmail.com>
2024-01-31 21:31:05 -0400

    chore(firebase): instalacion de dependencias de firebase

---
+++ src/app/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC-p68ldEb9hKzqXZzN5uufZheMcUSI9Ck",
+  authDomain: "prueba-auth-2ae86.firebaseapp.com",
+  projectId: "prueba-auth-2ae86",
+  storageBucket: "prueba-auth-2ae86.appspot.com",
+  messagingSenderId: "415556461049",
+  appId: "1:415556461049:web:51dba49609ef15dc9b1d28"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
